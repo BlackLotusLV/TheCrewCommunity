7db2c8784c54ad7acebcb4ff74ce49e0ccf3e27c
Tomass Polis-Lundgrēns <melnaislotoss@gmail.com>
2024-03-24 21:44:04 +0200

    Update user profile features and incorporate mod status
    Introduced an extended User profile element to include GlobalUsername, AvatarUrl and Mod status. A new CSS file for profile styling has been added. Profile data can now fetch from Discord API, while the status of a user being a moderator is determined based on their Discord roles. Some improvements were also made in account registering logic.

--- TheCrewCommunity/Pages/Account/Registering.cshtml.cs
+++ TheCrewCommunity/Pages/Account/Registering.cshtml.cs
@@ -1,22 +1,27 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Immutable;
+using System.Security.Claims;
+using DSharpPlus;
+using DSharpPlus.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using TheCrewCommunity.Data;
+using TheCrewCommunity.LiveBot;
 
 namespace TheCrewCommunity.Pages.Account;
 
 public class Registering : PageModel
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ILiveBotService _liveBotService;
+    private readonly ILogger<Registering> _logger;
     
-    Registering(UserManager<ApplicationUser> userManager)
+    public Registering(UserManager<ApplicationUser> userManager, ILiveBotService liveBotService, ILogger<Registering> logger)
     {
         _userManager = userManager;
-    }
-    
-    public void OnGet()
-    {
+        _liveBotService = liveBotService;
+        _logger = logger;
     }
 
     public async Task<IActionResult> OnGetAsync()
@@ -26,9 +31,93 @@ public class Registering : PageModel
         {
             return RedirectToAction("Index", "Home");
         }
+
+        ExtractMessageClaim(out string? discordIdString, out ulong discordId, out string? userName, out string? email);
+
+        if (discordIdString is null)
+        {
+            return BadRequest("Error getting Discord ID");
+        }
+
+        DiscordUser? discordUser = await FetchDiscordUserDetails(discordId);
+        if (!await CreateAndUpdateUser(discordId, userName, email, discordUser))
+        {
+            return BadRequest("Error creating or saving user");
+        }
+        return RedirectToAction("index", "Home");
+    }
+    private void ExtractMessageClaim(out string? discordIdString, out ulong discordId, out string userName, out string email)
+    {
         var claims = User.Identities
             .First().Claims
-            .Select(claim => new { claim.Issuer, claim.OriginalIssuer, claim.Type, claim.Value });
+            .Select(claim => new { claim.Issuer, claim.OriginalIssuer, claim.Type, claim.Value })
+            .ToImmutableList();
+        discordIdString = claims.FirstOrDefault(x => x.Type is ClaimTypes.NameIdentifier)?.Value;
+        discordId = Convert.ToUInt64(discordIdString);
+        userName = claims.First(x => x.Type is ClaimTypes.Name).Value;
+        email = claims.First(x => x.Type is ClaimTypes.Email).Value;
+    }
+    private async Task<DiscordUser?> FetchDiscordUserDetails(ulong discordId)
+    {
+        DiscordUser? discordUser = null;
+        try
+        {
+            discordUser = await _liveBotService.DiscordClient.GetUserAsync(discordId);
+        }
+        catch
+        {
+            _logger.LogDebug("Failed to get user of id {ID}", discordId);
+        }
+        return discordUser;
+    }
+    private async Task<bool> CreateAndUpdateUser(ulong discordId, string userName, string email, DiscordUser? discordUser)
+    {
+        var applicationUser = new ApplicationUser
+        {
+            DiscordId = discordId,
+            UserName = userName,
+            Email = email,
+            GlobalUsername = discordUser?.GlobalName,
+            AvatarUrl = discordUser?.AvatarUrl,
+            IsModerator = discordUser is not null && await ComputeModeratorStatus(discordUser, discordId)
+        };
+        IdentityResult createResult = await _userManager.CreateAsync(applicationUser);
+        if (!createResult.Succeeded)
+        {
+            return false;
+        }
+        IdentityResult saveResult = await _userManager.UpdateAsync(applicationUser);
+        return saveResult.Succeeded;
+    }
+    private async Task<bool> ComputeModeratorStatus(DiscordUser discordUser, ulong discordId)
+    {
+        DiscordMember? discordMember = null;
+        try
+        {
+            DiscordGuild guild = await _liveBotService.DiscordClient.GetGuildAsync(150283740172517376);
+            discordMember = await guild.GetMemberAsync(discordId);
+        }
+        catch
+        {
+            _logger.LogDebug("Failed to get member of id {ID}", discordId);
+        }
+
+        var roles = discordMember?.Roles ?? new List<DiscordRole>();
+        return roles.Any(role => role is { Permissions: Permissions.ModerateMembers });
+    }
+
+    /*
+    public async Task<IActionResult> OnGetAsyncold()
+    {
+        ApplicationUser? applicationUser = await _userManager.GetUserAsync(User);
+        if (applicationUser != null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        var claims = User.Identities
+            .First().Claims
+            .Select(claim => new { claim.Issuer, claim.OriginalIssuer, claim.Type, claim.Value })
+            .ToImmutableList();
         string? discordIdString = claims.FirstOrDefault(x=>x.Type is ClaimTypes.NameIdentifier)?.Value;
         if (discordIdString is null)
         {
@@ -38,13 +127,50 @@ public class Registering : PageModel
         
         string userName = claims.First(x=>x.Type is ClaimTypes.Name)?.Value??discordId.ToString();
         string email = claims.First(x=>x.Type is ClaimTypes.Email)?.Value??"";
-        
+
+        await using LiveBotDbContext dbContext = await _dbContextFactory.CreateDbContextAsync();
+        DiscordUser? discordUser = null;
+        DiscordMember? discordMember = null;
+        try
+        {
+            discordUser = await _liveBotService.DiscordClient.GetUserAsync(discordId);
+        }
+        catch
+        {
+            _logger.LogDebug("Failed to get user of id {ID}", discordId);
+        }
         applicationUser = new ApplicationUser
         {
             DiscordId = discordId,
             UserName = userName,
-            Email = email
+            Email = email,
+            GlobalUsername = null,
+            AvatarUrl = null,
+            IsModerator = false
         };
+        
+        if (discordUser is not null)
+        {
+            applicationUser.AvatarUrl = discordUser.AvatarUrl;
+            applicationUser.GlobalUsername = discordUser.GlobalName;
+            try
+            {
+                DiscordGuild guild = await _liveBotService.DiscordClient.GetGuildAsync(150283740172517376);
+                discordMember = await guild.GetMemberAsync(discordId);
+            }
+            catch
+            {
+                _logger.LogDebug("Failed to get member of id {ID}", discordId);
+            }
+
+            if (discordMember is not null)
+            {
+                var roles = discordMember.Roles;
+                bool isMod = roles.Any(role => role is { Permissions: Permissions.ModerateMembers });
+                applicationUser.IsModerator = isMod;
+            }
+        }
+        
         IdentityResult createResult = await _userManager.CreateAsync(applicationUser);
         if (!createResult.Succeeded)
         {
@@ -57,4 +183,5 @@ public class Registering : PageModel
         }
         return RedirectToAction("index", "Home");
     }
+    */
 }
