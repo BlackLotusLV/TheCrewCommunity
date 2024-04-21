96c8a66c6df9f353b16b0766c1f69fd99f1e5420
Tomass Polis-Lundgrēns <melnaislotoss@gmail.com>
2024-04-21 16:15:11 +0300

    Update website layout and add new ProSettings page
    Specific changes made to the site's CSS, including adding new classes for improved navigation and redesigning dropdown menus. A new Cars page was added under the Motorfest section's ProSettings.

---
+++ TheCrewCommunity/Pages/Motorfest/ProSettings/Cars.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheCrewCommunity.Pages.Motorfest.ProSettings;
+
+public class Cars : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
