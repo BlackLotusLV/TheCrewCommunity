9fcf40c33a88a649134f0f2b50e1ff9905369d42
Tomass Polis-Lundgrēns <melnaislotoss@gmail.com>
2024-09-28 19:06:44 +0300

    Implement client-side redirect after logout
    Added JavaScript in Logout.cshtml to redirect to home page upon logging out. Also updated the OnGet method to remove dependency on IActionResult.

--- TheCrewCommunity/Pages/Account/Logout.cshtml.cs
+++ TheCrewCommunity/Pages/Account/Logout.cshtml.cs
@@ -3,18 +3,18 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TheCrewCommunity.Data.WebData;
 
 namespace TheCrewCommunity.Pages.Account;
 
 public class Logout(SignInManager<ApplicationUser> signInManager, ILogger<Logout> logger) : PageModel
 {
-    public async Task<IActionResult> OnGet()
+    public async Task OnGet()
     {
         HttpContext.Session.Clear();
-        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
         await signInManager.SignOutAsync();
         logger.LogDebug("User logged out");
-        return Redirect("~/");
     }
 }
