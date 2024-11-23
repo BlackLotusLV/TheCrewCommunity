25f280c9c54f76ed9fb31382a87e5c6f1de73cf7
Tomass <melnaislotoss@gmail.com>
2024-11-23 15:11:40 +0200

    Add file size validation and new "This or That" form
    Introduce a `FileSizeLimitAttribute` for validating file size in uploads. Add a new Razor page for administrators to create "This or That" entries with detailed file and data validation.

---
+++ TheCrewCommunity/ValidationAttributes/FileSizeLimitAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheCrewCommunity.ValidationAttributes;
+
+public class FileSizeLimitAttribute(int maxFileSize) : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (ValidationResult.Success is null) throw new InvalidOperationException();
+        if (value is not IFormFile file) return ValidationResult.Success;
+        return file.Length > maxFileSize ? new ValidationResult(GetErrorMessage()) : ValidationResult.Success;
+    }
+    private string GetErrorMessage()
+    {
+        return $"Maximum allowed file size is { maxFileSize / 1024 / 1024} MB.";
+    }
+}
