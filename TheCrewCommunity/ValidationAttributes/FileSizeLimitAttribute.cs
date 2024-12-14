ed17900cc557200db33d190d67f69a97c913074e
Tomass <melnaislotoss@gmail.com>
2024-12-14 17:31:41 +0200

    Refactor file size validation to remove unnecessary check
    Removed redundant null check for `ValidationResult.Success`, streamlining the `IsValid` method. This change improves code clarity and ensures better maintainability.

--- TheCrewCommunity/ValidationAttributes/FileSizeLimitAttribute.cs
+++ TheCrewCommunity/ValidationAttributes/FileSizeLimitAttribute.cs
@@ -4,9 +4,8 @@ namespace TheCrewCommunity.ValidationAttributes;
 
 public class FileSizeLimitAttribute(int maxFileSize) : ValidationAttribute
 {
-    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (ValidationResult.Success is null) throw new InvalidOperationException();
         if (value is not IFormFile file) return ValidationResult.Success;
         return file.Length > maxFileSize ? new ValidationResult(GetErrorMessage()) : ValidationResult.Success;
     }
