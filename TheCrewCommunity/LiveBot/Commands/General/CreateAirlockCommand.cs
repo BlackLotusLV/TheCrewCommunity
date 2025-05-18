ec974ceda71230464607bf45fdd4913e1c6fd0e6
Tomass <melnaislotoss@gmail.com>
2025-05-18 19:07:20 +0300

    Add CreateAirlockCommand for server onboarding process
    This commit introduces the CreateAirlockCommand for guiding new users through joining the server. It includes placeholder functionality and assets for rules agreement and setting Ubisoft Connect usernames. Additional implementation is needed to complete the feature.

---
+++ TheCrewCommunity/LiveBot/Commands/General/CreateAirlockCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.ContextChecks;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Entities;
+
+namespace TheCrewCommunity.LiveBot.Commands.General;
+
+public class CreateAirlockCommand
+{
+    [Command("CreateAirlock"), RequireGuild, RequirePermissions(DiscordPermission.Administrator)]
+    public async Task CreateAirlock(SlashCommandContext ctx)
+    {
+        await ctx.RespondAsync("not implomented");
+
+        StringBuilder headText = new();
+        headText.AppendLine("# :traffic_light: AIRLOCK");
+        headText.AppendLine("Welcome! Before you can access the rest of the server:");
+        headText.AppendLine("️⃣ Agree to the server rules.");
+        headText.AppendLine("2️⃣ Set your Discord server profile nickname (https://support.discord.com/hc/en-us/articles/4409388345495-Server-Profiles) to your Ubisoft Connect username");
+        
+        DiscordMessageBuilder messageBuilder = new();
+        messageBuilder.EnableV2Components()
+            .AddTextDisplayComponent(new DiscordTextDisplayComponent(headText.ToString()))
+            .AddMediaGalleryComponent(new DiscordMediaGalleryItem(""), new DiscordMediaGalleryItem(""))
+    }
+}
