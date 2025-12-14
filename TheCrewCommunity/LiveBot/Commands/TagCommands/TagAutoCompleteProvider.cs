b3a11671d0f8c750c585b3c56678af40cebbc4c3
Tomass Polsi-Lundgrēns <melnaislotoss@gmail.com>
2025-12-15 00:19:18 +0200

    Refactor TagAutoCompleteProvider to enhance search accuracy by replacing Levenshtein distance with similarity scoring and simplifying input handling.

--- TheCrewCommunity/LiveBot/Commands/TagCommands/TagAutoCompleteProvider.cs
+++ TheCrewCommunity/LiveBot/Commands/TagCommands/TagAutoCompleteProvider.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Immutable;
-using System.Collections.ObjectModel;
-using DSharpPlus.Commands.Processors.SlashCommands;
+﻿using DSharpPlus.Commands.Processors.SlashCommands;
 using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
 using DSharpPlus.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -16,16 +14,17 @@ public class TagAutoCompleteProvider(IDbContextFactory<LiveBotDbContext> dbConte
         if (ctx.Guild is null) return new List<DiscordAutoCompleteChoice>();
         Guild guild = dbContext.Guilds.Include(x=>x.Tags).First(x=>x.Id == ctx.Guild.Id);
         if (guild.Tags is null) return new List<DiscordAutoCompleteChoice>();
-        string[] searchTokens = (ctx.UserInput.ToString() ?? "").ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        string userInput = ctx.UserInput ?? string.Empty;
         var tags = guild.Tags.Select(x =>
         {
-            string[] comparisonTokens = $"{x.Name} ({(x.Content.Length > 50 ? x.Content[..50] + "..." : x.Content)})".ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            int totalDistance = searchTokens.Sum(searchToken =>
-                comparisonTokens.Min(comparisonToken =>
-                    generalUtils.CalculateLevenshteinDistance(searchToken, comparisonToken)));
-            return (matchQuality: totalDistance, Result: x);
+            string preview = x.Content.Length > 50 ? x.Content[..50] + "..." : x.Content;
+            var combined = $"{x.Name} ({preview})";
+            double score = generalUtils.CalculateStringSimilarity(userInput, combined);
+            return (score, Result: x);
         });
-        var orderedTag = tags.OrderBy(x => x.matchQuality).Select(x => x.Result);
+        var orderedTag = tags
+            .OrderByDescending(x => x.score)
+            .Select(x => x.Result);
         return orderedTag.Select(tag => new DiscordAutoCompleteChoice($"{tag.Name} ({(tag.Content.Length > 50 ? tag.Content[..50] + "..." : tag.Content)})", tag.Id.ToString()));
     }
     
